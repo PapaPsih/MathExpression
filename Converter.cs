f7bb2345420d3bbbb8cc3a55661ca3c95665853d
PapaPsih <po4talehi007@rambler.ru>
2015-01-03 18:11:56 +0200

    Исправлена работа класса PriorityConverter

--- Converter.cs
+++ Converter.cs
@@ -13,14 +13,30 @@ namespace MathExpression
             this.expression = expression;
         }
 
+        private Int32  SearchIndex(string tmpExpression, Int32 MatchIndex)
+        {
+                int i=0;
+                for ( i=MatchIndex-1; tmpExpression[i] == ')'; i--);
+            return i;
+        }
         public String Result()
         {
-            Regex pattern = new Regex(@"\d+[\*\/]\d+");
+            //Regex pattern = new Regex(@"\(?\d+[\*\/]\d+\)?");
+            Regex pattern = new Regex(@"[\*\/]\d+\)?");
             String tmpExpression = this.expression.Substring(0);
             MatchCollection matches = pattern.Matches(tmpExpression);
-            foreach (Match item in matches)
+            for (int i = 0; i < matches.Count; i++)
             {
-                tmpExpression = tmpExpression.Replace(item.Value, String.Format("({0})", item.Value));
+                string temp = matches[i].Value;
+                Int32 num = SearchIndex(tmpExpression, matches[i].Index);
+                temp = tmpExpression.Substring(num, matches[i].Index - num + matches[i].Value.Length);
+
+                if ((num != 0 && tmpExpression[num - 1] != '(' )|| num == 0)
+                {
+                    tmpExpression = tmpExpression.Replace(temp, String.Format("({0})", temp));
+                    matches = pattern.Matches(tmpExpression);
+                }
+ 
             }
             return tmpExpression;
         }
--- Tests/UnitTest3.cs
+++ Tests/UnitTest3.cs
@@ -13,5 +13,37 @@ namespace MathExpressionTests
             PriorityConverter convert = new PriorityConverter(@"1+2*3");
             Assert.AreEqual("1+(2*3)", convert.Result());
         }
+        [TestMethod]
+        public void TestMethod2 ()
+        {
+            PriorityConverter convert = new PriorityConverter(@"(1+2)*3");
+            Assert.AreEqual("(1+(2)*3)", convert.Result());
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            PriorityConverter convert = new PriorityConverter(@"1+(2*3)");
+            Assert.AreEqual("1+(2*3)", convert.Result());
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            PriorityConverter convert = new PriorityConverter(@"((2+3)+4*5)+4/2");
+            Assert.AreEqual("((2+3)+(4*5))+(4/2)", convert.Result());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            PriorityConverter convert = new PriorityConverter(@"2*2*2");
+            Assert.AreEqual("(2*(2)*2)", convert.Result());
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            PriorityConverter convert = new PriorityConverter(@"((3+8)/(2*3))");
+            Assert.AreEqual("(2*(2)*2)", convert.Result());
+        }
     }
 }
